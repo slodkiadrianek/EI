78ff243fcf9f79485059102e4eadcfa73ea8d1b4
Adrian Kurek <adikurek11@gmail.com>
2025-08-05 21:52:04 +0200

    poprawki

--- models/element.go
+++ models/element.go
@@ -1,12 +1,12 @@
 package models
 
 type Element struct{
-	Id int
-	English string
-	Polish string
-	ExampleSentence string
-	Synonym string
-	SetId int
+	Id int `json:"id" sql:"id"`
+	English string `json:"english" sql:"english"`
+	Polish string 	`json:"polish" sql:"polish"`
+	ExampleSentence string `json:"exampleSentence" sql:"example_sentence"`
+	Synonym string `json:"synonym" sql:"synonym"`
+	SetId int `json:"setId" sql:"set_id"`
 }
 
 func NewElement(id, setId int, english, polish, exmapleSentence, synonym string) *Element{
